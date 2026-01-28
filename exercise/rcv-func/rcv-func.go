645d69378c122ece309f8cc01655d7144f126ff7
RustambekJavascript <rustambekeshboriyev@gmail.com>
2026-01-28 21:07:52 +0500

    feat: receiver function

--- exercise/rcv-func/rcv-func.go
+++ exercise/rcv-func/rcv-func.go
@@ -16,6 +16,59 @@ package main
 
 import "fmt"
 
+type Player struct {
+	Name      string
+	Health    int
+	MaxHealth int
+	Energy    int
+	MaxEnergy int
+}
+
+func (player *Player) setMaxHealth(amount int) {
+	oldHealth := player.Health
+	player.Health += amount
+
+	if player.Health > player.MaxHealth {
+		player.Health = player.MaxHealth
+	}
+
+	if player.Health < 0 {
+		player.Health = 0
+	}
+
+	fmt.Printf(
+		"%s ning Health o‘zgardi: %d -> %d (farq: %+d)\n",
+		player.Name, oldHealth, player.Health, amount,
+	)
+
+}
+
+func (player *Player) setMaxEnergy(amount int) {
+	oldEnergy := player.Energy
+	player.Energy += amount
+	if player.Energy > player.MaxEnergy {
+		player.Energy = player.MaxEnergy
+	}
+	if player.Energy < 0 {
+		player.Energy = 0
+	}
+
+	fmt.Printf(
+		"%s ning Health o‘zgardi: %d -> %d (farq: %+d)\n",
+		player.Name, oldEnergy, player.Energy, amount,
+	)
+}
+
 func main() {
 
+	player := Player{
+		Name:      "Russel",
+		Health:    34,
+		MaxHealth: 90,
+		Energy:    31,
+		MaxEnergy: 85,
+	}
+
+	player.setMaxHealth(10)
+	player.setMaxEnergy(50)
 }
